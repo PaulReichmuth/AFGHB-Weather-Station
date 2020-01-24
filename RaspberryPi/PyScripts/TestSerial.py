dcc42c44ab99bce7116540c7096abdf6f04d14a2
PaulReichmuth <p.n.reichmuth@gmail.com>
2020-01-24 21:07:10 +0100

    Made TestSerial.py runable

--- RaspberryPi/PyScripts/TestSerial.py
+++ RaspberryPi/PyScripts/TestSerial.py
@@ -1,6 +1,18 @@
-#!/usr/bin/python3
+##!/usr/bin/python3
 
-import pyserial as serial
+import serial
 from time import sleep as delay
 
+s = serial.Serial('/dev/ttyACM0', 115200)
+s.isOpen()
 
+try:
+	while True:
+		response = str(s.readline())
+		if "Wind" in response:
+			print("Direction " + response.strip("Wind "))
+		elif "Speed" in response:
+			print("Windspeed " + response.strip("Speed "))
+except KeyboardInterrupt:
+	s.close()
+	exit(0)
