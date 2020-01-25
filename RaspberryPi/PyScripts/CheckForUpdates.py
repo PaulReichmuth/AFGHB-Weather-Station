98aa954941229b3284eb9020a84f351e98c64132
PaulReichmuth <p.n.reichmuth@gmail.com>
2020-01-25 16:08:53 +0100

    move update script to master

---
+++ RaspberryPi/PyScripts/CheckForUpdates.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import sh
+from sh import git
+import time
+import os, sys
+
+aggregated = ""
+
+def CheckForUpdate(workingDir):
+    print("Fetching most recent code from source..." + workingDir)
+
+    # Fetch most up to date version of code.
+    p = git("--git-dir=" + workingDir + ".git/", "--work-tree=" + workingDir, "fetch", "origin", "testing", _out=ProcessFetch, _out_bufsize=0, _tty_in=True)
+    print("Fetch complete.")
+    time.sleep(2)
+    print("Checking status for " + workingDir + "...")
+    statusCheck = git("--git-dir=" + workingDir + ".git/", "--work-tree=" + workingDir, "status")
+
+    if "auf demselben Stand" in statusCheck:
+        print("Status check passes.")
+        print("Code up to date.")
+        return False
+    else:
+        print("Code update available.")
+        return True
+
+def ProcessFetch(char, stdin):
+    global aggregated
+
+    sys.stdout.flush()
+    aggregated += char
+    if aggregated.endswith("Password for 'https://yourrepo@bitbucket.org':"):
+        print(mainLogger, "Entering password...", True)
+        stdin.put("yourpassword\n")
+
+if __name__ == "__main__":
+    checkTimeSec = 60
+    gitDir = "/WeatherStation/"
+    while True:
+        print("*********** Checking for code update **************")
+        if CheckForUpdate(gitDir):
+            print("Resetting code...")
+            resetCheck = git("--git-dir=" + gitDir + ".git/", "--work-tree=" + gitDir, "reset", "--hard", "origin/testing")
+            print(str(resetCheck))
+        print("Check complete. Waiting for " + str(checkTimeSec) + "seconds until next check...", True)
+        time.sleep(checkTimeSec)
