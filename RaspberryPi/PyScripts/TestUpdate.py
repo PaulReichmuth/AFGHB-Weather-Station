6f4ffacc4093b3b3f9321abf55bd262e7b6438f5
PaulReichmuth <p.n.reichmuth@gmail.com>
2020-01-25 11:00:58 +0100

    Copied @johngrant´s Script in

--- RaspberryPi/PyScripts/TestUpdate.py
+++ RaspberryPi/PyScripts/TestUpdate.py
@@ -1,22 +1,44 @@
+import sh
 from sh import git
-from time import sleep as delay
+import time
+import os, sys
 
+aggregated = ""
 
-git.fetch()
-status = git.status()
+def CheckForUpdate(workingDir):
+    print("Fetching most recent code from source..." + workingDir)
 
-try:
-	while True:
-		print(status)
-		if not "auf demselben Stand" in status:
-			print(git.pull())
-			print("updated")
-			print("\n"+"\n"+"\n"+"\n"+"\n"+"\n")
-			delay(10)
-		else:
-			print("no updates available")
-			delay(10)
-			git.fetch()
-			status = git.status()
-except KeyboardInterrupt:
-	exit()
+    # Fetch most up to date version of code.
+    p = git("--git-dir=" + workingDir + ".git/", "--work-tree=" + workingDir, "fetch", "origin", "master", _out=ProcessFetch, _out_bufsize=0, _tty_in=True)
+    print("Fetch complete.")
+    time.sleep(2)
+    print("Checking status for " + workingDir + "...")
+    statusCheck = git("--git-dir=" + workingDir + ".git/", "--work-tree=" + workingDir, "status")
+
+    if "auf dem selben Stand" in statusCheck:
+        print("Status check passes.")
+        print("Code up to date.")
+        return False
+    else:
+        print("Code update available.")
+        return True
+
+def ProcessFetch(char, stdin):
+    global aggregated
+
+    sys.stdout.flush()
+    aggregated += char
+    if aggregated.endswith("Password for 'https://yourrepo@bitbucket.org':"):
+        print(mainLogger, "Entering password...", True)
+        stdin.put("yourpassword\n")
+
+if __name__ == "__main__":    checkTimeSec = 60
+    gitDir = "/WeatherStation/"
+    while True:
+        print("*********** Checking for code update **************")
+        if CheckForUpdate(gitDir):
+            print("Resetting code...")
+            resetCheck = git("--git-dir=" + gitDir + ".git/", "--work-tree=" + gitDir, "reset", "--hard", "origin/master")
+            print(str(resetCheck))
+        print("Check complete. Waiting for " + str(checkTimeSec) + "seconds until next check...", True)
+        time.sleep(checkTimeSec)
