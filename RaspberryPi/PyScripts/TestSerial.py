1ca01fd0123e50bf27fb4b2f62caf7ed9325eea2
PaulReichmuth <p.n.reichmuth@gmail.com>
2020-01-25 20:04:17 +0100

    some stuff

--- RaspberryPi/PyScripts/TestSerial.py
+++ RaspberryPi/PyScripts/TestSerial.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
+import paho.mqtt.publish as pub
 import serial
 from time import sleep as delay
+
 
 s = serial.Serial('/dev/ttyACM0', 115200)
 s.isOpen()
@@ -11,8 +12,10 @@ try:
 		response = str(s.readline())
 		if "Wind" in response:
 			print("Direction " + response.strip("Wind "))
+			pub.single("sensors/data/wind/direction", response.strip("Wind "))
 		elif "Speed" in response:
 			print("Windspeed " + response.strip("Speed "))
+			pub.single("sensors/data/wind/speed", response.strip("Speed "))
 except KeyboardInterrupt:
 	s.close()
 	exit(0)
