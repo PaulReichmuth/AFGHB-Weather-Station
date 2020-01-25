c73fd6f61928f400a5d9cdd441ac04163cc784fa
PaulReichmuth <p.n.reichmuth@gmail.com>
2020-01-25 10:46:23 +0100

    First Script

--- RaspberryPi/PyScripts/TestUpdate.py
+++ RaspberryPi/PyScripts/TestUpdate.py
@@ -1,1 +1,22 @@
 from sh import git
+from time import sleep as delay
+
+
+git.fetch()
+status = git.status()
+
+try:
+	while True:
+		print(status)
+		if not "auf demselben Stand" in status:
+			print(git.pull())
+			print("updated")
+			print("\n"+"\n"+"\n"+"\n"+"\n"+"\n")
+			delay(10)
+		else:
+			print("no updates available")
+			delay(10)
+			git.fetch()
+			status = git.status()
+except KeyboardInterrupt:
+	exit()
