e7a85db5ad2a75ac523e50fd0e4746da67bc78f9
Paul Reichmuth <PaulReichmuth@users.noreply.github.com>
2020-01-25 20:03:26 +0100

    Update TestSerial.ino

--- Arduino/scripts/TestSerial/TestSerial.ino
+++ Arduino/scripts/TestSerial/TestSerial.ino
@@ -1,4 +1,5 @@
 String dir[8] = {"N","NE","E","SE","S","SW","W","NW"};
+int dirnum = 0;
 
 void setup() {
 Serial.begin(115200);
@@ -10,9 +11,12 @@ delay(1000);
 
 void loop() {
 Serial.print("Wind ");
-Serial.println(dir[random(8)]);
+Serial.println(dirnum);
 Serial.print("Speed ");
 Serial.println(random(analogRead(2)));
 delay(2500);
-
+dirnum ++;
+if(dirnum > 8){
+  dirnum = 0;
+}
 }
