7f37af5308eefbbeed526a6c3f3cfc4769552452
PaulReichmuth <p.n.reichmuth@gmail.com>
2020-05-03 17:36:19 +0200

    Added telegram functionality

--- RaspberryPi/PyScripts/CheckForUpdates.py
+++ RaspberryPi/PyScripts/CheckForUpdates.py
@@ -6,6 +6,8 @@ from sh import git
 import time
 import os, sys
 import argparse
+import requests
+
 
 parser = argparse.ArgumentParser(description='Update a git directory from a selected branch')
 parser.add_argument('branch', metavar='branch', type=str ,help='branch to update from')
@@ -18,6 +20,15 @@ print ("directory = " + args.dir)
 aggregated = ""
 branch = args.branch
 gitDir = args.dir
+
+def telegram_notify(bot_message):
+    bot_token = '1198116152:AAGnSQK62eVTi1Lz1D5_8SHXkhJ_q0tF_4U'
+    bot_chatID = '413144530'
+    send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
+
+    response = requests.get(send_text)
+
+    return response.json()
 
 def CheckForUpdate(workingDir):
     print("Fetching most recent code from source..." + workingDir)
@@ -54,5 +65,7 @@ if __name__ == "__main__":
             print("Resetting code...")
             resetCheck = git("--git-dir=" + gitDir + ".git/", "--work-tree=" + gitDir, "reset", "--hard", "origin/" + branch)
             print(str(resetCheck))
+            message = "Update applied:" + "\n" + str(resetCheck)
+            telegram_notify(message)
         print("Check complete. Waiting for " + str(checkTimeSec) + "seconds until next check...")
         time.sleep(checkTimeSec)
