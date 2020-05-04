790592350368a61b265760b003b2907cc616cecf
PaulReichmuth <p.n.reichmuth@gmail.com>
2020-05-04 15:33:47 +0100

    Adapted to linux

--- RaspberryPi/PyScripts/TestSysLog.py
+++ RaspberryPi/PyScripts/TestSysLog.py
@@ -19,7 +19,7 @@ load = psutil.cpu_percent(interval=0.5)
 freq = psutil.cpu_freq().current
 cores = "Running on a {} core system".format(psutil.cpu_count(logical=False))
 boottime = datetime.datetime.fromtimestamp(psutil.boot_time()).strftime("Gestartet um %H:%M:%S am %A, den %d %B %Y")
-eth0addr = psutil.net_if_addrs()['Ethernet'][1].address
+eth0addr = psutil.net_if_addrs()['eth0'][1].address
 net_tx = psutil.net_io_counters().bytes_sent
 net_rx = psutil.net_io_counters().bytes_recv
 net_tx_err = psutil.net_io_counters().errout
