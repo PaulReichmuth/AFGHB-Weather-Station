04cef7b99f15dc63dd0cca7f81f5a88e1116cb02
Paul Reichmuth <PaulReichmuth@users.noreply.github.com>
2020-05-04 13:06:06 +0200

    just some more stuff

--- RaspberryPi/PyScripts/TestSysLog.py
+++ RaspberryPi/PyScripts/TestSysLog.py
@@ -1,5 +1,9 @@
 import psutil
 load = psutil.cpu_percent(interval=1)
+diskc = psutil.disk_usage("C:")
+diskd = psutil.disk_usage("D:")
 
 
-print(load)
+print("CPU: " + str(load))
+print("Disk C: " + str(diskc.percent))
+print("Disk D: " + str(diskd.percent))
