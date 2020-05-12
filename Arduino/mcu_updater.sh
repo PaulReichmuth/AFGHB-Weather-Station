0f5740b47073a164f888c1d5794facd8730225ba
PaulReichmuth <p.n.reichmuth@gmail.com>
2020-05-12 21:25:26 +0200

    maybe fixed updater

--- Arduino/mcu_updater.sh
+++ Arduino/mcu_updater.sh
@@ -1,2 +1,3 @@
+cd /mnt/d/Paul/Documents/Github/AFGHB-Weather-Station/Arduino/ #change to git dir
 arduino-cli compile -b sensebox:samd:sb update && arduino-cli upload -p /dev/ACM0 -b sensebox:samd:sb update
 exit 0
