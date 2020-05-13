a9a3d5b385d137f0ee5b8a2e758b69485db068d9
Paul Reichmuth <50195869+PaulReichmuth@users.noreply.github.com>
2020-05-13 10:44:10 +0200

    Update mcu_updater.sh

--- Arduino/mcu_updater.sh
+++ Arduino/mcu_updater.sh
@@ -1,3 +1,3 @@
 cd /home/WeatherStation/mcu_updateable/Arduino/ #change to git dir
-arduino-cli compile -b sensebox:samd:sb update && arduino-cli upload -p /dev/ACM0 -b sensebox:samd:sb update
+arduino-cli compile -b sensebox:samd:sb update && arduino-cli upload -p /dev/ttyACM0 -b sensebox:samd:sb update
 exit 0
