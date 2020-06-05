b591750efa8d5bc4429cbff2c9e9b33db334d00c
Paul Reichmuth <50195869+PaulReichmuth@users.noreply.github.com>
2020-06-05 18:23:00 +0200

    Update TestSerial.py

--- RaspberryPi/PyScripts/TestSerial.py
+++ RaspberryPi/PyScripts/TestSerial.py
@@ -11,17 +11,17 @@ try:
 	while True:
 		response = str(s.readline())
 		if "Wind" in response:
-			print("Direction " + re.findall(r"[-+]?\d*\.\d+|\d+",response))
-			pub.single("sensors/data/wind/direction", re.findall(r"[-+]?\d*\.\d+|\d+",response))
+			print("Direction " + re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
+			pub.single("sensors/data/wind/direction", re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
 		elif "Speed" in response:
-			print("Windspeed " + re.findall(r"[-+]?\d*\.\d+|\d+",response))
-			pub.single("sensors/data/wind/speed", re.findall(r"[-+]?\d*\.\d+|\d+",response))
+			print("Windspeed " + re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
+			pub.single("sensors/data/wind/speed", re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
 		elif "Temp" in response:
-			print("Temperature " + re.findall(r"[-+]?\d*\.\d+|\d+",response))
-			pub.single("sensors/data/atmo/temp", re.findall(r"[-+]?\d*\.\d+|\d+",response))
+			print("Temperature " + re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
+			pub.single("sensors/data/atmo/temp", re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
 		elif "Humi" in response:
-			print("Humidity " + re.findall(r"[-+]?\d*\.\d+|\d+",response))
-			pub.single("sensors/data/atmo/humi", re.findall(r"[-+]?\d*\.\d+|\d+",response))
+			print("Humidity " + re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
+			pub.single("sensors/data/atmo/humi", re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
 except KeyboardInterrupt:
 	s.close()
 	exit(0)
