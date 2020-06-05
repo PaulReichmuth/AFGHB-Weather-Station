d22782cb7506b169fd61ce9f1bef35870bfd7bc6
Paul Reichmuth <50195869+PaulReichmuth@users.noreply.github.com>
2020-06-05 17:55:11 +0200

    Added different float extraction method

--- RaspberryPi/PyScripts/TestSerial.py
+++ RaspberryPi/PyScripts/TestSerial.py
@@ -2,7 +2,7 @@
 import paho.mqtt.publish as pub
 import serial
 from time import sleep as delay
-
+import re
 
 s = serial.Serial('/dev/ttyACM0', 115200)
 s.isOpen()
@@ -11,17 +11,17 @@ try:
 	while True:
 		response = str(s.readline())
 		if "Wind" in response:
-			print("Direction " + response.strip("Wind "))
-			pub.single("sensors/data/wind/direction", response.strip("Wind "))
+			print("Direction " + re.findall(r"[-+]?\d*\.\d+|\d+",response))
+			pub.single("sensors/data/wind/direction", re.findall(r"[-+]?\d*\.\d+|\d+",response))
 		elif "Speed" in response:
-			print("Windspeed " + response.strip("Speed "))
-			pub.single("sensors/data/wind/speed", response.strip("Speed "))
+			print("Windspeed " + re.findall(r"[-+]?\d*\.\d+|\d+",response))
+			pub.single("sensors/data/wind/speed", re.findall(r"[-+]?\d*\.\d+|\d+",response))
 		elif "Temp" in response:
-			print("Temperature " + response.strip("Temp "))
-			pub.single("sensors/data/atmo/temp", response.strip("Temp "))
+			print("Temperature " + re.findall(r"[-+]?\d*\.\d+|\d+",response))
+			pub.single("sensors/data/atmo/temp", re.findall(r"[-+]?\d*\.\d+|\d+",response))
 		elif "Humi" in response:
-			print("Humidity " + response.strip("Humi "))
-			pub.single("sensors/data/atmo/humi", response.strip("Humi "))
+			print("Humidity " + re.findall(r"[-+]?\d*\.\d+|\d+",response))
+			pub.single("sensors/data/atmo/humi", re.findall(r"[-+]?\d*\.\d+|\d+",response))
 except KeyboardInterrupt:
 	s.close()
 	exit(0)
