1d23b200ecd2a434e782bfa4913de125a61c0c86
Paul Reichmuth <50195869+PaulReichmuth@users.noreply.github.com>
2020-06-09 17:59:07 +0200

    Fixed Major FUCKUP

--- Arduino/mcu_updater.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-cd /home/WeatherStation/mcu_updateable/Arduino/ #change to git dir
-/home/WeatherStation/bin/arduino-cli compile -b sensebox:samd:sb update && arduino-cli upload -p /dev/ttyACM0 -b sensebox:samd:sb update
-
--- monitor.py
+++
@@ -1,6 +0,0 @@
-import serial
-
-s = serial.Serial("/dev/ttyACM0",115200)
-
-while s.isOpen():
-	print(s.readline())
