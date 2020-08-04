f6318e133dce6bc1a71b82e4b90980c424df2173
Paul Reichmuth <PaulReichmuth@users.noreply.github.com>
2020-08-04 09:29:05 +0200

    Managed String to Float Conversion from re.findall

--- RaspberryPi/PyScripts/TestSerial.py
+++ RaspberryPi/PyScripts/TestSerial.py
@@ -16,8 +16,8 @@ try:
 	while True:
 		response = str(s.readline())
 		if "Wind" in response:
-			print("Direction " + re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
-			direction = re.findall(r"[-+]?\d*\.\d+|\d+",response)[0]
+			print("Direction " + float(re.findall(r"[-+]?\d*\.\d+|\d+",response)[0]))
+			direction = float(re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
 			ifdb = "Weather_Wind"
 			name = "Direction"
 			body = [
@@ -35,8 +35,8 @@ try:
 			ifclient.write_points(body)
 			ifclient.close()
 		elif "Speed" in response:
-			print("Windspeed " + re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
-			speed = re.findall(r"[-+]?\d*\.\d+|\d+",response)[0]
+			print("Windspeed " + float(re.findall(r"[-+]?\d*\.\d+|\d+",response)[0]))
+			speed = float(re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
 			ifdb = "Weather_Wind"
 			name = "Speed"
 			body = [
@@ -53,8 +53,8 @@ try:
 			ifclient.write_points(body)
 			ifclient.close()
 		elif "Temp" in response:
-			print("Temperature " + re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
-			temp = re.findall(r"[-+]?\d*\.\d+|\d+",response)[0]
+			print("Temperature " + float(re.findall(r"[-+]?\d*\.\d+|\d+",response)[0]))
+			temp = float(re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
 			ifdb = "Weather_Air"
 			name = "Temperature"
 			body = [
@@ -71,8 +71,8 @@ try:
 			ifclient.write_points(body)
 			ifclient.close()
 		elif "Humi" in response:
-			print("Humidity " + re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
-			humi = re.findall(r"[-+]?\d*\.\d+|\d+",response)[0]
+			print("Humidity " + float(re.findall(r"[-+]?\d*\.\d+|\d+",response)[0]))
+			humi = float(re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
 			ifdb = "Weather_Air"
 			name = "Humidity"
 			body = [
@@ -89,8 +89,8 @@ try:
 			ifclient.write_points(body)
 			ifclient.close()
 		elif "Lux" in response:
-			print("Lux " + re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
-			lux = re.findall(r"[-+]?\d*\.\d+|\d+",response)[0]
+			print("Lux " + float(re.findall(r"[-+]?\d*\.\d+|\d+",response)[0]))
+			lux = float(re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
 			ifdb = "Weather_Sun"
 			name = "Light Intensity"
 			body = [
@@ -100,6 +100,42 @@ try:
         			"fields": {
 						"value":lux,
 						"Unit":"lux"
+					}
+				}
+			]
+			ifclient = InfluxDBClient(ifhost,ifport,ifuser,password=None,database=ifdb)
+			ifclient.write_points(body)
+			ifclient.close()
+		elif "Pressure" in response:
+    		pressure = float(re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
+			print("Air Pressure" + pressure)
+			ifdb = "Weather_Air"
+			name = "Pressure"
+			body = [
+				{
+					"measurement": name,
+
+					"fields": {
+						"value": pressure,
+						"Unit": "hPa"
+					}
+				}
+			]
+			ifclient = InfluxDBClient(ifhost,ifport,ifuser,password=None,database=ifdb)
+			ifclient.write_points(body)
+			ifclient.close()
+		elif "Reference Temp" in response:
+    		reftemp = float(re.findall(r"[-+]?\d*\.\d+|\d+",response)[0])
+			print("Reference Temp" + reftemp)
+			ifdb = "Weather_Air"
+			name = "Temp ref."
+			body = [
+				{
+					"measurement": name,
+
+					"fields": {
+						"value": reftemp,
+						"Unit": "°C"
 					}
 				}
 			]
